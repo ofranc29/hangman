a283df071a84195f0af1288ed068f55473060035
ofrancis <otisfrancis1@gmail.com>
2022-12-16 22:40:09 +0000

    Updated text in the help and readme files

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 - Pick a letter from the letter pad, on the right-hand side of the screen.
 - For each incorrect letter that you select an element of the hangman will appear in the img on the left-hand side of the screen.
 - If you select 10 incorrect letters you lose the game and you will be moved onto the next word.
-- If you get the word correct, you will be awarded 10 pts.
-- If at any point you get stuck you can select the "Hint" button to get a single correct letter given to you. Hoever this will dock your points total by 2pts.
+- If you get the word correct, you will be awarded 10 pts and moved onto the next word.
+- If at any point you get stuck you can select the "Hint" button to get a single correct letter given to you. However this will dock your points total by 2pts.
 - If you hit the "Restart" button, this will reset the entire game and you will lose all of your points and progress.
 - Happy gaming!
 
--- src/Help/Help.js
+++ src/Help/Help.js
@@ -9,8 +9,8 @@ const Help = () => {
                 <li className='rules-list-item'>Pick a letter from the letter pad, on the right-hand side of the screen.</li>
                 <li className='rules-list-item'>For each incorrect letter that you select an element of the hangman will appear in the img on the left-hand side of the screen.</li>
                 <li className='rules-list-item'>If you select 10 incorrect letters you lose the game and you will be moved onto the next word.</li>
-                <li className='rules-list-item'>If you get the word correct, you will be awarded 10 pts.</li>
-                <li className='rules-list-item'>If at any point you get stuck you can select the "Hint" button to get a single correct letter given to you. Hoever this will dock your points total by 2pts.</li>
+                <li className='rules-list-item'>If you get the word correct, you will be awarded 10 pts and you will be moved on to the next word.</li>
+                <li className='rules-list-item'>If at any point you get stuck you can select the "Hint" button to get a single correct letter given to you. However this will dock your points total by 2pts.</li>
                 <li className='rules-list-item'>If you hit the "Restart" button, this will reset the entire game and you will lose all of your points and progress.</li>
                 <li className='rules-list-item'>Happy gaming!</li>
             </ul>
